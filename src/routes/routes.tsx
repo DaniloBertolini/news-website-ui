c83b28376eb6b78413cb376701c752dced241663
Danilo <danilocbertolini@gmail.com>
2023-09-20 18:48:15 -0300

    Remoção about

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "../pages/Home"
-import About from "../pages/About"
 import Profile from "../pages/Profile"
 import Layout from "../components/Layout"
 
@@ -10,7 +9,6 @@ function routes() {
       <Routes>
         <Route path="/" Component={ Layout } >
           <Route index Component={ Home } />
-          <Route path="about" Component={ About } />
           <Route path="profile" Component={ Profile } />
         </Route>
       </Routes>
