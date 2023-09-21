47526923101ce0537562de62b6aef351742bef6d
Danilo <danilocbertolini@gmail.com>
2023-09-21 12:51:14 -0300

    animação SVG profile

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,24 @@
 import profileIcon from '/profile.svg'
 import website2Icon from '/website2.svg'
+import usersIcon from '/users.svg'
+import { useState } from 'react'
 
 function NavBar() {
+  const [icon, setIcon] = useState(profileIcon)
+  const handleChange = () => {
+    if (icon === profileIcon) {
+      setIcon(usersIcon)
+    } else {
+      setIcon(profileIcon)
+    }    
+  }
+
   return (
     <div className="relative">
       <nav className="bg-cyan-900 flex justify-between p-6 fixed top-0 left-0 right-0">
         <img className="shadow-lg" src={ website2Icon } alt="svg navbar" />
         <div>
-          <img src={ profileIcon } alt="svg profile" />
+          <img onMouseEnter={ handleChange } onMouseLeave={ handleChange }  src={ icon } alt="svg profile" />
         </div>
       </nav>
     </div>
